dd831e5dd779db924efeb160efe3a9d794d75495
Pedro <pemoraes1@gmail.com>
2019-11-26 15:46:34 -0300

    Criando arquivo e estruturando as funções para implementação do algoritmo.

---
+++ escritor-leitor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void entraLeitura() {}
+
+void saiLeitura() {}
+
+void entraEscrita() {}
+
+void saiEscrita() {}
+
+void *leitora(void *arg) {}
+
+void *escritora(void *arg) {}
+
+void main(int argc, char *argv[]) {}
